0fb26b1271157a4ed2d5d10a1816106fe95ef839
mauriop <michalis.mavriopoulos@gmail.com>
2023-09-24 22:41:47 +0300

    feat: process with rxjs

--- pub/index.js
+++ pub/index.js
@@ -4,15 +4,34 @@ const express = require("express");
 
 const redis = new Redis();
 
+const sleep = (ms, cb) => new Promise(() => setTimeout(cb, ms));
+
 const app = express();
-// app.use(json());
+
+app.use(express.json());
 
 app.post("/publish", (req, res) => {
+  const timestamp = Date.now();
   //   redis.publish("send-user-data", JSON.stringify({ ts: new Date() }));
-  redis.xadd("mystream", "*", "id", new Date());
-  return res.json({ msg: "I hope this runs 😅" });
+  redis.xadd("mystream", "*", "id", timestamp);
+  return res.json({ msg: `I hope this runs 😅, ${timestamp}` });
+});
+
+app.post("/ack", (req, res) => {
+  console.log(req.body);
+  const { id } = req.body;
+  sleep(5000, () => {
+    console.log("acked", id);
+    res.status(201).json({ msg: `acked id: ${id}` });
+  });
 });
 
 app.listen(4000, () => {
+  try {
+    redis.xgroup("CREATE", "mystream", "mygroup", "$", "MKSTREAM");
+  } catch (err) {
+    console.log(err);
+  }
+
   console.log("server started");
 });
--- sub/index.js
+++ sub/index.js
@@ -1,23 +1,46 @@
-// @/sub/index.js
-
 const Redis = require("ioredis");
+const { Observable, timer, from, of, interval } = require("rxjs");
+const {
+  map,
+  concatMap,
+  delay,
+  mergeMap,
+  switchMap,
+  catchError,
+} = require("rxjs/operators");
+const os = require("os");
+const axios = require("axios");
 
 const redis = new Redis();
 
-const main = () => {
-  redis.subscribe("send-user-data", (err, count) => {
-    if (err) console.error(err.message);
-    console.log(`Subscribed to ${count} channels.`);
-  });
+const sleep = (ms, cb) => new Promise(() => setTimeout(cb, ms));
 
-  redis.on("message", (channel, message) => {
-    console.log(`Received message from ${channel} channel.`);
-    console.log(JSON.parse(message));
-  });
+const url = "http://localhost:4000/ack";
+
+const customHeaders = {
+  "Content-Type": "application/json",
 };
 
-const processMessage = (message) => {
-  console.log("Id: %s. Data: %O", message[0], message[1]);
+const processMessage = async (message) => {
+  const [id, data] = message.messages[0];
+  console.log("to ack message with Id: %s. Data: %O", id, data);
+
+  await redis.xack("mystream", "mygroup", id);
+};
+
+const postAck = async (message) => {
+  const [id, data] = message?.messages[0];
+  console.log("to post ack message with Id: %s", id);
+  const response = await axios.post(
+    url,
+    { id },
+    {
+      headers: customHeaders,
+    }
+  );
+  console.log(response.body);
+
+  return response;
 };
 
 async function listenForMessage(lastId = "$") {
@@ -26,7 +49,18 @@ async function listenForMessage(lastId = "$") {
   // a single element. See more: https://redis.io/commands/xread#return-value
 
   // see here: https://javascript.plainenglish.io/an-introduction-to-redis-stream-57445a21751e
-  const results = await redis.xreadgroup("BLOCK", 0, "GROUP", "mygroup", process.pid, "COUNT", 1, "STREAMS", "mystream", ">");
+  const results = await redis.xreadgroup(
+    "BLOCK",
+    0,
+    "GROUP",
+    "mygroup",
+    os.hostname,
+    "COUNT",
+    1,
+    "STREAMS",
+    "mystream",
+    ">"
+  );
   const [key, messages] = results[0]; // `key` equals to "mystream"
 
   messages.forEach(processMessage);
@@ -35,6 +69,84 @@ async function listenForMessage(lastId = "$") {
   await listenForMessage(messages[messages.length - 1][0]);
 }
 
-listenForMessage();
+// listenForMessage();
 
-// main();
+const read = async () => {
+  console.log("reading...");
+  const res = await redis.xreadgroup(
+    "BLOCK",
+    2000,
+    "GROUP",
+    "mygroup",
+    `${os.hostname()} - ${process.pid}`,
+    "COUNT",
+    1,
+    "STREAMS",
+    "mystream",
+    ">"
+  );
+
+  if (!res || !res.length) {
+    return undefined;
+  }
+  const [key, messages] = res[0];
+  const result = {
+    key,
+    messages,
+  };
+
+  return result;
+};
+
+const executeAsyncTasks = async (message) => {
+  // Replace with your async tasks to external resources
+  console.log("Executing async tasks for response:", message);
+  // Simulate an async task with a delay
+  const body = await postAck(message);
+  if (!body) {
+    throw new Error("error");
+  }
+  await processMessage(message);
+  console.log("Async tasks executed for response:", message);
+};
+
+// timer(0, 500)
+// interval(500)
+//   .pipe(
+//     concatMap(() =>
+//       from(read()).pipe(
+//         map((response) => {
+//           console.log("response", response);
+//           return response;
+//         })
+//       )
+//     )
+//   )
+//   .pipe(
+//     concatMap(async (item) => {
+//       await processMessage(item);
+//     })
+//   )
+//   // .pipe(map(async (m) => await processMessage(m)))
+//   .subscribe((m) => console.log(m));
+interval(500)
+  .pipe(
+    concatMap(() =>
+      from(read()).pipe(
+        concatMap((response) =>
+          of(response).pipe(
+            concatMap((responseData) => from(executeAsyncTasks(responseData))),
+            catchError((error) => {
+              console.error("Error executing async work:", error);
+              return of(null);
+            })
+          )
+        ),
+        catchError((error) => {
+          console.error("Error fetching data:", error);
+          return of(null);
+        })
+      )
+    )
+  )
+  .subscribe((x) => console.log(x));
